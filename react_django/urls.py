b952354ec6046754e973f5674790311725ad0700
Michael White <hrt666.cs@gmail.com>
2023-06-01 01:53:51 +0900

    fix: add as_view() to ReactView to be callable

--- react_django/urls.py
+++ react_django/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ReactView(), name="ReactView")
+    path('', ReactView.as_view(), name="ReactView")
 ]

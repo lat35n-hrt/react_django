c2fc85265deee5d53c72fddd4abe305c937e378b
Michael White <hrt666.cs@gmail.com>
2023-06-01 20:21:17 +0900

    feat: Add serializer_class attribute to ReactView in views.py

--- app/views.py
+++ app/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import *
-from .serializer import *
+from .models import React
+from .serializer import ReactSerializer
 from rest_framework.response import Response
 
 
 class ReactView(APIView):
+
+    serializer_class = ReactSerializer
+
     def get(self, request):
         output = [{"employment": output.employee,
                    "department": output.department}
                    for output in React.objects.all()]
         return Response(output)
+    
     def post(self, request):
         serializer = ReactSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save
+            serializer.save()
             return Response(serializer.data)
-        
